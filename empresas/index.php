2234822a5db3293502e4b6d10d94516ce1e7ece1
Ellivelton Narciso <ellivelton.souza@gmail.com>
2022-06-01 18:05:32 -0400

    ok

---
+++ empresas/index.php
@@ -0,0 +1,37 @@
+<?php
+    
+$matriz['cliente_fornecedor_Cod'] = "cliente_fornecedor_Cod";
+$matriz['cliente_fornecedor'] = "cliente_fornecedor";
+$matriz['tipoPessoaCod'] = "tipoPessoaCod";
+$matriz['tipoPessoa'] = "tipoPessoa";
+$matriz['razaoSocial'] = "razaoSocial";
+$matriz['nomeFantasia'] = "nomeFantasia";
+$matriz['cnpj_cpf'] = "cnpj_cpf";
+
+$matriz['logradouro'] = "logradouro";
+$matriz['logradouroNumero'] = "logradouroNumero";
+$matriz['logradouroComplemento'] = "logradouroComplemento";
+$matriz['bairro'] = "Bairro";
+
+$matriz['telefone'][1] = "6733331111";
+$matriz['telefone'][2] = "6733332222";
+$matriz['telefone'][3] = "6733333333";
+$matriz['telefone'][3] = "6733334444";
+
+$matriz['email'][1] = "email1@email.com";
+$matriz['email'][2] = "email2@email.com";
+$matriz['email'][3] = "email3@email.com";
+$matriz['email'][3] = "email4@email.com";
+
+$matriz['gerenciamentoRiscoCod'] = "gerenciamentoRiscoCod";
+$matriz['gerenciamentoRisco'] = "gerenciamentoRisco";
+$matriz['qtdEntidades'] = "qtdEntidades";
+$matriz['situacaoCod'] = "situacaoCod";
+$matriz['situacaoTexto'] = "situacaoTexto";
+$matriz['situacaoIcon'] = "situacaoIcon";
+$matriz['situacaoColor'] = "situacaoColor";
+
+
+    echo json_encode($matriz);
+
+?>
